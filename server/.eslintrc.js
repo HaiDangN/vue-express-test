142a9a532f5e2fbda5b8525ea5023c21cf3c32f1
HaiDangN <haidangnguyenus@gmail.com>
2023-06-28 15:14:38 -0700

    setup of express js server

---
+++ server/.eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:vue/vue3-essential"
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules": {
+    }
+}
