06fe98e22b8bb2b804caa0744153948cd69a47e6
Hai Dang Nguyen <haidangn@uw.edu>
2023-07-05 10:18:20 -0700

    vuetify

--- client/src/main.js
+++ client/src/main.js
@@ -6,7 +6,13 @@ import App from './App.vue'
 import Home from '@/views/Home.vue'
 import About from '@/views/About.vue'
 import Register from '@/views/Register.vue'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
+
+  
 
 const routes = [
     {path: '/', name: 'Home', component: Home},
@@ -20,6 +26,11 @@ const router = createRouter({
     routes
 })
 
-createApp(App)
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+
+createApp(App).use(vuetify)
 .use(router)
 .mount('#app')
--- client/src/views/Register.vue
+++ client/src/views/Register.vue
@@ -1,27 +1,83 @@
 <template>
-    <div class="home">
-        <h1>Register</h1>
-        <br>
-        <input 
-            type="email" 
-            name="email"
-            v-model="email"
-            placeholder="email" />
-        <br>
-        <input 
-            type="password" 
-            name="password"
-            v-model="password"
-            placeholder="password" />
-        <br>
-        <div class="error" v-html="error" />
-        <br>
-        <button
-            @click="register">
-            Register
-        </button>
+    <div class="login-container">
+      <v-img
+        class="mx-auto my-6"
+        max-width="228"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Sound_Transit_logo.svg/440px-Sound_Transit_logo.svg.png"
+      ></v-img>
+  
+      <v-card
+        class="login-form"
+        elevation="8"
+        max-width="448"
+        rounded="lg"
+      >
+        <div class="text-subtitle-1 text-medium-emphasis">Account</div>
+  
+        <v-text-field
+          density="compact"
+          placeholder="Email address"
+          prepend-inner-icon="mdi-email-outline"
+          variant="outlined"
+        ></v-text-field>
+  
+        <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">
+          Password
+  
+          <a
+            class="text-caption text-decoration-none text-blue"
+            href="#"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            Forgot login password?</a>
+        </div>
+  
+        <v-text-field
+          :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
+          :type="visible ? 'text' : 'password'"
+          density="compact"
+          placeholder="Enter your password"
+          prepend-inner-icon="mdi-lock-outline"
+          variant="outlined"
+          @click:append-inner="visible = !visible"
+        ></v-text-field>
+  
+        <v-card
+          class="mb-12"
+          color="surface-variant"
+          variant="tonal"
+        >
+          <v-card-text class="text-medium-emphasis text-caption">
+            Password requirements:
+            <br>Alpha-numeric characters only
+            <br>8-32 characters
+          </v-card-text>
+        </v-card>
+  
+        <v-btn
+          block
+          class="mb-8"
+          color="blue"
+          size="large"
+          variant="tonal"
+        >
+          Log In
+        </v-btn>
+  
+        <v-card-text class="text-center">
+          <a
+            class="text-blue text-decoration-none"
+            href="#"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            Sign up now <v-icon icon="mdi-chevron-right"></v-icon>
+          </a>
+        </v-card-text>
+      </v-card>
     </div>
-</template>
+  </template>
 
 <script>
 import AuthenticationService from '@/services/AuthenticationService.js'
@@ -47,8 +103,19 @@ export default {
     }
 }
 </script>
+
 <style scoped>
-.error {
-    color: red;
+.login-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+.login-form {
+  width: 100%;
+  max-width: 448px;
+  padding: 12px;
+  margin: 0 auto;
 }
 </style>
